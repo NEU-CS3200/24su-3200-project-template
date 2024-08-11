c3e7dc21a9dc137431a1f20f206bfe6e30fff85f
hayliepedersen <pedersenhaylie6@gmail.com>
2024-08-10 22:01:11 -0400

    added data to schemas'

---
+++ database-files/01_petalytics-data.sql
@@ -0,0 +1,155 @@
+USE petalytics;
+
+--
+-- Data for table 'agencies'
+-- 
+
+INSERT INTO agencies VALUES
+	('1', '806-771-2867', 'dkleinholz0@reference.com', 'Yakitri', '469 Northfield Center', 'Amarillo', 'Texas', '13561'),
+	('2', '707-630-0979', 'mitzkov1@mapy.cz', 'Photobug', '2441 Linden Crossing', 'Santa Rosa', 'California', '15003'),
+	('3', '801-816-9548', 'slimpricht2@dagondesign.com', 'Jaloo', '0806 Linden Alley', 'Salt Lake City', 'Utah', '13141'),
+	('4', '740-370-5233', 'moshesnan3@facebook.com', 'Snaptags', '8253 Westport Drive', 'Columbus', 'Ohio', '17557'),
+	('5', '559-572-4613', 'kallenson4@nydailynews.com', 'Buzzbean', '52 Algoma Plaza', 'Fresno', 'California', '12509'),
+	('6', '540-451-6886', 'fmantrip5@rakuten.co.jp', 'Vimbo', '10932 Glendale Avenue', 'Roanoke', 'Virginia', '15446'),
+	('7', '512-680-9821', 'korrey6@pcworld.com', 'Yabox', '56 Loftsgordon Center', 'Austin', 'Texas', '11378'),
+	('8', '336-133-4199', 'jbus7@independent.co.uk', 'Realpoint', '658 Hagan Street', 'High Point', 'North Carolina', '12431'),
+	('9', '615-371-9186', 'thumphrys8@un.org', 'Fivechat', '5 Sloan Hill', 'Nashville', 'Tennessee', '16073'),
+	('10', '405-953-5257', 'drogliero9@google.co.uk', 'Kwimbee', '7911 Meadow Ridge Way', 'Oklahoma City', 'Oklahoma', '15561'),
+	('11', '714-276-5318', 'fwillicotta@house.gov', 'Skinder', '7207 Russell Pass', 'Anaheim', 'California', '12354'),
+	('12', '818-957-3741', 'jparsonageb@army.mil', 'Tagpad', '3 Butterfield Avenue', 'Glendale', 'California', '19739'),
+	('13', '208-961-6447', 'mwrightsonc@wikimedia.org', 'Zoomdog', '44518 Clarendon Way', 'Boise', 'Idaho', '12180'),
+	('14', '813-717-7352', 'mchurnd@163.com', 'Bluejam', '68 Delladonna Trail', 'Tampa', 'Florida', '19560'),
+	('15', '303-172-6534', 'sfelixe@state.gov', 'Digitube', '9122 Express Pass', 'Aurora', 'Colorado', '17513'),
+	('16', '309-520-3171', 'ikitchinerf@alibaba.com', 'Roodel', '1626 Eagan Crossing', 'Carol Stream', 'Illinois', '13952'),
+	('17', '916-301-8474', 'ckondratovichg@ebay.co.uk', 'Pixope', '60 Amoth Avenue', 'Sacramento', 'California', '13070'),
+	('18', '915-732-0228', 'erylstoneh@hexun.com', 'Babbleset', '15179 Birchwood Drive', 'El Paso', 'Texas', '17067'),
+	('19', '850-614-8763', 'tglossopi@drupal.org', 'Avamm', '54 Upham Street', 'Pensacola', 'Florida', '17002'),
+	('20', '314-443-5318', 'atumaj@multiply.com', 'Edgeblab', '41 Mayfield Circle', 'Saint Louis', 'Missouri', '19950'),
+	('21', '216-363-4026', 'jginmank@storify.com', 'Blogpad', '307 Del Sol Crossing', 'Cleveland', 'Ohio', '14280'),
+	('22', '501-435-2451', 'lcordoval@cnbc.com', 'Trupe', '93 Claremont Lane', 'Little Rock', 'Arkansas', '16924'),
+	('23', '434-552-6137', 'skubickam@theguardian.com', 'Blogtag', '0 Talisman Park', 'Lynchburg', 'Virginia', '13475'),
+	('24', '216-845-3378', 'aparnhamn@stumbleupon.com', 'Oyonder', '95764 Sunnyside Circle', 'Cleveland', 'Ohio', '14259'),
+	('25', '662-319-8057', 'fbrammallo@spiegel.de', 'Gigaclub', '37089 Talmadge Point', 'Columbus', 'Mississippi', '10134'),
+	('26', '559-975-4987', 'dmarousekp@list-manage.com', 'Plajo', '806 Hoffman Way', 'Fresno', 'California', '14724'),
+	('27', '865-586-5125', 'ghatcherq@chicagotribune.com', 'Meevee', '39374 Loeprich Center', 'Knoxville', 'Tennessee', '13753'),
+	('28', '865-179-3350', 'acotmorer@123-reg.co.uk', 'Dablist', '3 South Trail', 'Knoxville', 'Tennessee', '10563'),
+	('29', '210-627-0073', 'llethlays@macromedia.com', 'Browsecat', '02 Londonderry Center', 'San Antonio', 'Texas', '19823'),
+	('30', '323-686-7103', 'rgirathst@auda.org.au', 'Agivu', '87902 Schurz Junction', 'North Hollywood', 'California', '15572'),
+	('31', '812-180-3553', 'aealdenu@reference.com', 'Blogtags', '2 Glacier Hill Avenue', 'Terre Haute', 'Indiana', '15017'),
+	('32', '260-485-0314', 'aforringtonv@merriam-webster.com', 'Yabox', '470 Westend Drive', 'Fort Wayne', 'Indiana', '14944'),
+	('33', '202-194-7812', 'cstokesw@comcast.net', 'Edgetag', '370 Kennedy Point', 'Washington', 'District of Columbia', '12780'),
+	('34', '786-901-1881', 'hjosifovicx@digg.com', 'Rooxo', '50886 Prentice Street', 'Miami', 'Florida', '13402'),
+	('35', '313-841-3567', 'rtaddy@alexa.com', 'Photofeed', '14734 Springview Parkway', 'Detroit', 'Michigan', '16082'),
+	('36', '860-329-9772', 'lnoltonz@mlb.com', 'Eayo', '781 Pennsylvania Point', 'Hartford', 'Connecticut', '15103'),
+	('37', '561-718-6901', 'karch10@narod.ru', 'Ooba', '4600 Northland Park', 'Pompano Beach', 'Florida', '17364'),
+	('38', '361-824-1684', 'fmaceur11@discuz.net', 'Tazzy', '4 Welch Center', 'Corpus Christi', 'Texas', '16031'),
+	('39', '312-999-0918', 'teliassen12@epa.gov', 'Flashpoint', '488 Gulseth Lane', 'Aurora', 'Illinois', '13042'),
+	('40', '404-319-7300', 'iwarrillow13@google.es', 'Janyx', '51829 Mcbride Point', 'Atlanta', 'Georgia', '17685');
+
+--
+-- Data for table 'pets'
+-- 
+
+INSERT INTO pets VALUES
+	('1', 'Bendicty', 'false', 'false', 'cat', 'Siamese', '2018', '6'),
+	('2', 'Meagan', 'false', 'false', 'cat', 'Siamese', '2009', '15'),
+	('3', 'Ursola', 'true', 'false', 'cat', 'Sphynx', '2019', '5'),
+	('4', 'Bert', 'false', 'true', 'dog', 'Labrador Retriever', '2009', '15'),
+	('5', 'Bjorn', 'true', 'true', 'dog', 'Golden Retriever', '2014', '10'),
+	('6', 'Kevin', 'false', 'true', 'dog', 'Husky', '2018', '6'),
+	('7', 'Rustin', 'false', 'true', 'dog', 'Terrier', '2009', '15'),
+	('8', 'Aida', 'false', 'true', 'dog', 'Poodle', '2010', '14'),
+	('9', 'Trevar', 'true', 'true', 'cat', 'Sphynx', '2022', '2'),
+	('10', 'Myrle', 'true', 'false', 'cat', 'Maine Coon', '2018', '6'),
+	('11', 'Maria', 'false', 'false', 'dog', 'Maine Coon', '2017', '7'),
+	('12', 'Devland', 'false', 'true', 'dog', 'Poodle', '2021', '3'),
+	('13', 'Sax', 'true', 'false', 'dog', 'Poodle', '2013', '11'),
+	('14', 'Elbertine', 'false', 'false', 'dog', 'Husky', '2024', '0'),
+	('15', 'Arne', 'true', 'false', 'dog', 'Bulldog', '2014', '10'),
+	('16', 'Gabriel', 'true', 'false', 'cat', 'Maine Coon', '2019', '5'),
+	('17', 'Gabbie', 'true', 'true', 'dog', 'Bulldog', '2014', '10'),
+	('18', 'Wynn', 'false', 'true', 'cat', 'Maine Coon', '2019', '5'),
+	('19', 'Augusta', 'false', 'true', 'dog', 'Bulldog', '2015', '9'),
+	('20', 'Regine', 'true', 'true', 'dog', 'Husky', '2021', '3'),
+	('21', 'Barret', 'false', 'true', 'dog', 'Husky', '2024', '0'),
+	('22', 'Madge', 'true', 'true', 'cat', 'Ragdoll', '2015', '9'),
+	('23', 'Abe', 'false', 'false', 'cat', 'Sphynx', '2021', '3'),
+	('24', 'Miguel', 'true', 'true', 'dog', 'Labrador Retriever', '2019', '5'),
+	('25', 'Morgen', 'false', 'true', 'dog', 'Golden Retriever', '2014', '10'),
+	('26', 'Roddy', 'true', 'false', 'cat', 'Maine Coon', '2010', '14'),
+	('27', 'Margalo', 'true', 'false', 'dog', 'Maine Coon', '2016', '8'),
+	('28', 'Gardiner', 'false', 'true', 'cat', 'Siamese', '2018', '6'),
+	('29', 'Aggy', 'true', 'true', 'cat', 'Calico', '2012', '12'),
+	('30', 'Inesita', 'false', 'true', 'cat', 'Calico', '2015', '9'),
+	('31', 'Codie', 'false', 'false', 'cat', 'Ragdoll', '2008', '16'),
+	('32', 'Bibbye', 'false', 'true', 'dog', 'Golden Retriever', '2015', '9'),
+	('33', 'Laurice', 'true', 'true', 'cat', 'Shorthair', '2008', '16'),
+	('34', 'Aurthur', 'true', 'true', 'cat', 'Persian', '2012', '12'),
+	('35', 'Cesaro', 'true', 'false', 'cat', 'Sphynx', '2014', '10'),
+	('36', 'Olwen', 'false', 'true', 'dog', 'Chocolate Lab', '2009', '15'),
+	('37', 'Blythe', 'false', 'false', 'cat', 'Persian', '2008', '16'),
+	('38', 'Jolynn', 'false', 'true', 'dog', 'Bulldog', '2022', '2'),
+	('39', 'Carrie', 'false', 'true', 'dog', 'Burmese', '2024', '0'),
+	('40', 'Peggi', 'false', 'false', 'cat', 'Golden Retriever', '2020', '4');
+
+--
+-- Data for table 'managers'
+-- 
+
+INSERT INTO pets VALUES
+	('1', '38', '728-869-2935', 'Waite', 'Moloney', 'wmoloney0@sitemeter.com'),
+	('2', '2', '140-638-5424', 'Beale', 'Ghidoli', 'bghidoli1@reverbnation.com'),
+	('3', '34', '349-993-8166', 'Hymie', 'Chatelain', 'hchatelain2@live.com'),
+	('4', '3', '292-801-7826', 'Brnaby', 'Peace', 'bpeace3@comsenz.com'),
+	('5', '22', '103-303-0399', 'Stewart', 'Geraudy', 'sgeraudy4@tripadvisor.com'),
+	('6', '40', '852-604-4961', 'Lorry', 'MacGraith', 'lmacgraith5@bandcamp.com'),
+	('7', '13', '201-269-3334', 'Ester', 'Loeber', 'eloeber6@51.la'),
+	('8', '1', '367-327-0419', 'Erick', 'Pettifer', 'epettifer7@photobucket.com'),
+	('9', '22', '490-512-1844', 'Prentice', 'Meanwell', 'pmeanwell8@cam.ac.uk'),
+	('10', '22', '214-492-1268', 'Nichol', 'Fergyson', 'nfergyson9@tamu.edu'),
+	('11', '23', '830-927-2524', 'Shirleen', 'Sheepy', 'ssheepya@mtv.com'),
+	('12', '5', '364-955-3766', 'Fulton', 'Andrassy', 'fandrassyb@miibeian.gov.cn'),
+	('13', '40', '995-644-5689', 'Lorianna', 'Phizaclea', 'lphizacleac@issuu.com'),
+	('14', '9', '480-805-0012', 'Marga', 'Cuxson', 'mcuxsond@drupal.org'),
+	('15', '39', '200-417-6114', 'Cora', 'Simondson', 'csimondsone@stanford.edu'),
+	('16', '4', '264-909-0399', 'Penny', 'Breakey', 'pbreakeyf@home.pl'),
+	('17', '4', '344-149-7921', 'Denver', 'Lody', 'dlodyg@wp.com'),
+	('18', '19', '674-777-8904', 'Catherina', 'Leipoldt', 'cleipoldth@amazonaws.com'),
+	('19', '35', '178-686-2657', 'Darrin', 'Bryant', 'dbryanti@lycos.com'),
+	('20', '36', '303-769-0997', 'Hedy', 'Channer', 'hchannerj@businesswire.com'),
+	('21', '6', '578-145-3457', 'Cordula', 'Zeplin', 'czeplink@digg.com'),
+	('22', '36', '146-837-9999', 'Oona', 'Griniov', 'ogriniovl@reference.com'),
+	('23', '22', '496-479-9176', 'Juan', 'Talmadge', 'jtalmadgem@ebay.co.uk'),
+	('24', '27', '558-242-2959', 'Nealson', 'Vaskov', 'nvaskovn@aol.com'),
+	('25', '26', '725-330-0181', 'Ring', 'Seaborne', 'rseaborneo@netlog.com'),
+	('26', '19', '371-779-8108', 'Zaria', 'Routhorn', 'zrouthornp@blogspot.com'),
+	('27', '1', '120-110-5448', 'Bethina', 'Jizhaki', 'bjizhakiq@booking.com'),
+	('28', '3', '147-463-7413', 'Wolfie', 'Kares', 'wkaresr@economist.com'),
+	('29', '30', '124-347-0658', 'Maurizia', 'Stygall', 'mstygalls@google.co.jp'),
+	('30', '2', '642-901-0656', 'Aurilia', 'Wrankmore', 'awrankmoret@cyberchimps.com'),
+	('31', '18', '409-809-0348', 'Lissi', 'Spraggon', 'lspraggonu@wp.com'),
+	('32', '6', '369-170-9326', 'Collen', 'Sein', 'cseinv@hubpages.com'),
+	('33', '10', '941-250-2729', 'Muhammad', 'Wasbey', 'mwasbeyw@latimes.com'),
+	('34', '24', '306-189-1756', 'Lenette', 'Hauger', 'lhaugerx@furl.net'),
+	('35', '8', '251-938-9040', 'Jone', 'Edgell', 'jedgelly@webeden.co.uk'),
+	('36', '4', '795-351-4963', 'Tam', 'Eustes', 'teustesz@elegantthemes.com'),
+	('37', '32', '811-851-1080', 'Karyl', 'Stealey', 'kstealey10@nasa.gov'),
+	('38', '17', '167-633-0637', 'Malchy', 'Pulfer', 'mpulfer11@dot.gov'),
+	('39', '22', '416-634-9541', 'Roy', 'Dalgleish', 'rdalgleish12@boston.com'),
+	('40', '20', '839-945-0632', 'Jakob', 'Malec', 'jmalec13@about.com');
+
+--
+-- Data for table 'pet_agencies'
+-- 
+
+--
+-- Data for table 'adopters'
+-- 
+
+--
+-- Data for table 'adoptions'
+-- 
+
+--
+-- Data for table 'medical_records'
+-- 
