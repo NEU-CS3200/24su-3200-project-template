0ffcdf5cd1633e0b61fe36f95f058c354aee73b1
Caroline Pham <carolinetpham@outlook.com>
2024-08-12 14:40:54 -0400

    adding pet data for adopter story 1

--- app/src/pages/01_Pet_Data_Viz.py
+++ app/src/pages/01_Pet_Data_Viz.py
@@ -14,48 +14,50 @@ import requests
 SideBarLinks()
 
 # set the header of the page
-st.header('Adoptable Pet Data d')
+st.header('Adoptable Pet Data')
 
 # You can access the session state to make a more customized/personalized app experience
 st.write(f"### Hi, {st.session_state['first_name']}.")
 
-response = request.get('http://api:4000/p/pets').json()
+# get the pets from the database
+pets = requests.get('http://localhost:4000/p/pets').json()
 
-pet_data = response.json()
+try: 
+    pets = [pet for pet in pets if pet['adoption_status'] == 0 and pet['is_alive'] == 1]
+    st.dataframe(pets)
+except: 
+    st.write('Could not connect to database to retrieve pet data')    
 
-st.write(pet_data)
 
-'''
 # get the countries from the world bank data
-with st.echo(code_location='above'):
-    countries:pd.DataFrame = wb.get_countries()
+# with st.echo(code_location='above'):
+#    countries:pd.DataFrame = wb.get_countries()
    
-    st.dataframe(countries)
+#    st.dataframe(countries)
 
 # the with statment shows the code for this block above it 
-with st.echo(code_location='above'):
-    arr = np.random.normal(1, 1, size=100)
-    test_plot, ax = plt.subplots()
-    ax.hist(arr, bins=20)
+# with st.echo(code_location='above'):
+#    arr = np.random.normal(1, 1, size=100)
+#    test_plot, ax = plt.subplots()
+#    ax.hist(arr, bins=20)
 
-    st.pyplot(test_plot)
+#    st.pyplot(test_plot)
 
 
-with st.echo(code_location='above'):
-    slim_countries = countries[countries['incomeLevel'] != 'Aggregates']
-    data_crosstab = pd.crosstab(slim_countries['region'], 
-                                slim_countries['incomeLevel'],  
-                                margins = False) 
-    st.table(data_crosstab)
+#with st.echo(code_location='above'):
+#    slim_countries = countries[countries['incomeLevel'] != 'Aggregates']
+#    data_crosstab = pd.crosstab(slim_countries['region'], 
+#                                slim_countries['incomeLevel'],  
+#                                margins = False) 
+#    st.table(data_crosstab)
 
-if st.button('See aniamals',
-             type='primary',
-             use_container_width=True):
-  results = requests.get('http://api:4000/p/pets').json()
-  st.dataframe(results)
-if st.button('See agencies',
-             type='primary',
-             use_container_width=True):
-  results = requests.get('http://api:4000/a/agencies').json()
-  st.dataframe(results)
-  '''
+#if st.button('See aniamals',
+#             type='primary',
+#             use_container_width=True):
+#  results = requests.get('http://api:4000/p/pets').json()
+#  st.dataframe(results)
+#if st.button('See agencies',
+#             type='primary',
+#             use_container_width=True):
+#  results = requests.get('http://api:4000/a/agencies').json()
+#  st.dataframe(results)
