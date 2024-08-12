5ac7b1823846bf798bac8fab9191dd66350dc79a
brookeblanton12 <brookekb1@gmail.com>
2024-08-12 12:32:55 -0400

    made sure new data is working

--- app/src/pages/11_Area_Price.py
+++ app/src/pages/11_Area_Price.py
@@ -12,18 +12,24 @@ SideBarLinks()
 
 st.title('# View Price of Homes Based on Area')
 
-url = 'http://localhost:4000/l/tenMostExpensive'
-try:
-    response = requests.get(url)
-    if response.status_code == 200:
-        all_data = response.json()
-        st.write("Successfully connected to the API.")
-        st.write("ten most expensive listings:")
-        st.dataframe(all_data)  # Displaying all user data in a dataframe
-    else:
-        st.error(f"Failed to retrieve all users. Status code: {response.status_code}")
-        st.text("Response:" + response.text)
-except requests.exceptions.RequestException as e:
-    st.error("An error occurred while trying to connect to the API to fetch all users:")
-    st.text(str(e))
+listing_state = st.text_input("Enter Listings state to fetch specific listings details", "")
 
+url = 'http://localhost:4000/l/tenLeastExpensive'
+
+# Conditionally make API request based on user input for specific user details
+if listing_state:
+    specific_url = f"{url}/{listing_state}"
+    try:
+        response = requests.get(specific_url)
+        if response.status_code == 200:
+            user_data = response.json()
+            st.write("Successfully fetched data listing state:")
+            st.json(user_data)  # Displaying specific user data in JSON format for clarity
+        else:
+            st.error(f"Failed to retrieve data for listing ID {listing_state}. Status code: {response.status_code}")
+            st.text("Response:" + response.text)
+    except requests.exceptions.RequestException as e:
+        st.error(f"An error occurred while trying to connect to the API to fetch listing ID {listing_state}:")
+        st.text(str(e))
+
+        
