967b65b53e422fd0bfeab7e78149fa71d37a1d18
Bryan Guen <guen.br@northeastern.edu>
2024-08-12 12:43:40 -0400

    back to default

--- api/backend/customers/customer_routes.py
+++ api/backend/customers/customer_routes.py
@@ -72,19 +72,3 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
-
-# Get customer detail for customer with particular userID
-@customers.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
-    current_app.logger.info('GET /customers/<userID> route')
-    cursor = db.get_db().cursor()
-    cursor.execute('select id, first_name, last_name from customers where id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
