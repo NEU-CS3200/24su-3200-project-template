65bc78853dcdc09350739883c0e54c16d489653b
Mark Fontenot <m.fontenot@northeastern.edu>
2024-08-12 06:16:34 -0400

    Updated Home.py with comments and enhanced logging

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,26 +1,60 @@
+##################################################
+# This is the main/entry-point file for the 
+# sample application for your project
+##################################################
+
+# Set up basic logging infrastructure
 import logging
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# import the main streamlit library as well
+# as SideBarLinks function from src/modules folder
 import streamlit as st
 from modules.nav import SideBarLinks
 
+# streamlit supports reguarl and wide layout (how the controls
+# are organized/displayed on the screen).
 st.set_page_config(layout = 'wide')
 
+# If a user is at this page, we assume they are not 
+# authenticated.  So we change the 'authenticated' value
+# in the streamlit session_state to false. 
 st.session_state['authenticated'] = False
+
+# Use the SideBarLinks function from src/modules/nav.py to control
+# the links displayed on the left-side panel. 
+# IMPORTANT: ensure src/.streamlit/config.toml sets
+# showSidebarNavigation = false in the [client] section
 SideBarLinks(show_home=True)
 
-st.title('The Profs App')
+# ***************************************************
+#    The major content of this page
+# ***************************************************
 
+# set the title of the page and provide a simple prompt. 
+logger.info("Loading the Home page of the app")
+st.title('The Profs App')
 st.write('\n\n')
 st.write('### HI! As which user would you like to log in?')
+
+# For each of the user personas for which we are implementing
+# functionality, we put a button on the screen that the user 
+# can click to MIMIC logging in as that mock user. 
 
 if st.button("Act as John, a Political Strategy Advisor", 
             type = 'primary', 
             use_container_width=True):
+    # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
+    # we set the role of the current user
     st.session_state['role'] = 'pol_strat_advisor'
+    # we add the first name of the user (so it can be displayed on 
+    # subsequent pages). 
     st.session_state['first_name'] = 'John'
+    # finally, we ask streamlit to switch to another page, in this case, the 
+    # landing page for this particular user type
+    logger.info("Logging in as Political Strategy Advisor Persona")
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
 if st.button('Act as Mohammad, an USAID worker', 
