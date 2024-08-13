c128551d2c489ee29a732c9c9d7a177255222aa0
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-13 12:07:17 -0400

    add student sam persona

--- app/src/Home.py
+++ app/src/Home.py
@@ -34,28 +34,31 @@ SideBarLinks(show_home=True)
 
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('The Profs App')
+st.title('Project Pal')
 st.write('\n\n')
 st.write('### HI! As which user would you like to log in?')
 
 # For each of the user personas for which we are implementing
 # functionality, we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
+# Student Sam
+# Professor Mark 
+# TA Timmy 
 
-if st.button("Act as John, a Political Strategy Advisor", 
+if st.button("Act as Sam, a NEU Student", 
             type = 'primary', 
             use_container_width=True):
     # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
     # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
+    st.session_state['role'] = 'student'
     # we add the first name of the user (so it can be displayed on 
     # subsequent pages). 
-    st.session_state['first_name'] = 'John'
+    st.session_state['first_name'] = 'Sam'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+    logger.info("Logging in as Student Persona")
+    st.switch_page('pages/00_Pol_Strat_Home.py') # --- need to change this
 
 if st.button('Act as Mohammad, an USAID worker', 
             type = 'primary', 
--- app/src/pages/00_Pol_Strat_Home.py
+++ app/src/pages/00_Pol_Strat_Home.py
@@ -9,7 +9,7 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome Political Strategist, {st.session_state['first_name']}.")
+st.title(f"Welcome Student, {st.session_state['first_name']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
