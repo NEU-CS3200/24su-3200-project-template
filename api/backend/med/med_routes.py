500341aa2f6e356e384d6746de403aa47bed8a27
Andrew Nee <andrewnee29@gmail.com>
2024-08-13 13:59:05 -0400

    update med_routes
    updated med route post function

--- api/backend/med/med_routes.py
+++ api/backend/med/med_routes.py
@@ -1,20 +1,19 @@
 ########################################################
-# Sample pets blueprint of endpoints
+# Sample medical history blueprint of endpoints
 # Remove this file if you are not using it in your project
 ########################################################
 from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from backend.db_connection import db
 
-pets = Blueprint('pets', __name__)
+pets = Blueprint('med', __name__)
 
-# Get all pets from the DB
-@pets.route('/pets', methods=['GET'])
-def get_pets():
-    current_app.logger.info('pets_routes.py: GET /pets')
+# Get all medical history entries from a specific pet
+@med.route('/med/<petID>', methods=['GET'])
+def med_pets(petID):
+    current_app.logger.info('med_routes.py: GET /med')
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT petID, name, adoption_status,\
-        species, breed, birthday, age, is_alive FROM pets')
+    cursor.execute('SELECT petID, entryNumber, entry, date FROM medical_record WHERE petID = ' + str(petID))
 
     theData = cursor.fetchall()
     the_response = make_response(theData)
@@ -22,73 +21,19 @@ def get_pets():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Add a new pet to the DB
-@pets.route('/pets', methods=['POST'])
-def add_pet():
-    current_app.logger.info('POST /pets route')
-    pet_info = request.json
-    petID = pet_info['petID']
-    name = pet_info['name']
-    status = pet_info['adoption_status']
-    species = pet_info['species']
-    breed = pet_info['breed']
-    birthday = pet_info['birthday']
-    age = pet_info['age']
-    alive = pet_info['is_alive']
+# Add a new entry in the pet medical record
+@med.route('/med/<petID>', methods=['POST'])
+def add_entry():
+    current_app.logger.info('POST /med route')
+    med_info = request.json
+    entry = med_info['entry']
+    petID = med_info['petID']
+    date = med_info['date']
 
-    query = 'INSERT INTO pets (petID, name, adoption_status, species, breed, birthday, age, is_alive) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
-    data = (petID, name, status, species, breed, birthday, age, alive)
+    query = 'INSERT INTO medical_record (entry, petID, date) VALUES (%s, %s, %s)'
+    data = (entry, petID, date)
     cursor = db.get_db().cursor()
     r = cursor.execute(query, data)
     db.get_db().commit()
-    return 'pet added!'  
+    return 'medical entry added!'  
 
-@pets.route('/pets', methods=['PUT'])
-def update_pets():
-    current_app.logger.info('PUT /pets route')
-    pet_info = request.json
-    # current_app.logger.info(pet_info)
-    petID = pet_info['petID']
-    name = pet_info['name']
-    status = pet_info['adoption_status']
-    species = pet_info['species']
-    breed = pet_info['breed']
-    birthday = pet_info['birthday']
-    age = pet_info['age']
-    alive = pet_info['is_alive']
-
-
-    query = 'UPDATE pets SET name = %s, adoption_status = %s, species = %s, breed = %s, birthday = %s,\
-             age = %s, is_alive = %s WHERE petID = %s'
-    data = (petID, name, status, species, breed, birthday, age, alive)
-    cursor = db.get_db().cursor()
-    r = cursor.execute(query, data)
-    db.get_db().commit()
-    return 'customer updated!'
-
-# Get pet detail for a pet with particular petID
-@pets.route('/pets/<petID>', methods=['GET'])
-def get_pet(petID):
-    current_app.logger.info('GET /pets/<petID> route')
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT name, adoption_status, species, breed, birthday, age, is_alive FROM pets WHERE petID = ' + str(petID))
-
-    theData = cursor.fetchall()
-
-    the_response = make_response(theData)
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-# Get all avilable Pets for potention adopter viewing
-@pets.route('/pets/available', methods=['GET'])
-def get_available_pets():
-    current_app.logger.info('pets_routes.py: GET /pets/available')
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT petID, name,\
-        species, breed, age FROM pets WHERE is_alive = 1 AND adoption_status = 0')
-
-    theData = cursor.fetchall()
-    the_response = make_response(theData)
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
