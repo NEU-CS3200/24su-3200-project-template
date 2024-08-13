f640dc96aa414b0dfc0796b1ead1ba728a184d48
Caroline Pham <carolinetpham@outlook.com>
2024-08-13 17:02:41 -0400

    changing to matplotlib instead of plotly

--- app/src/pages/13_Manager_Adoption_View.py
+++ app/src/pages/13_Manager_Adoption_View.py
@@ -7,12 +7,11 @@ from sklearn.ensemble import RandomForestClassifier
 from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
 import requests
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 SideBarLinks()
 
 st.write("# View All Adoption Data")
-st.write(f"## Hi, {st.session_state['first_name']}!")
 adoption_data = requests.get('http://api:4000/adp/adoptions').json()
 df = pd.DataFrame(adoption_data)
 df = df[['name', 'petID', 'firstName', 'lastName', 'adopterID', 'email', 'phone', 'adoption_date',\
@@ -26,14 +25,12 @@ adoption_status = df['adoptionStatus'].value_counts()
 # Set colors
 colors = ['indianred', 'lightgreen', 'gold']
 
-fig = go.Figure(data=[go.Bar(
-    x=adoption_status.index,
-    y=adoption_status.values,
-    marker_color=colors,  
-    width=0.5
-)])
+plt.figure(figsize=(8, 4)) 
+plt.bar(adoption_status.index, adoption_status.values, color=colors)
+plt.ylabel('Count')
+plt.xticks(rotation='horizontal')
 
-st.plotly_chart(fig)
+st.pyplot(plt)
 
 # Displaying the chart after the graph
 st.write("### Chart of all Adoption Data")
