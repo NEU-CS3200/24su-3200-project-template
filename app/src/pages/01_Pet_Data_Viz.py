447df48fddc29705ca379c11037c544338acedea
leraseti <bryce.mottershead@gmail.com>
2024-08-13 13:57:39 -0400

    added filtering to pet data viz page

--- app/src/pages/01_Pet_Data_Viz.py
+++ app/src/pages/01_Pet_Data_Viz.py
@@ -3,8 +3,6 @@ logger = logging.getLogger(__name__)
 import pandas as pd
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
-import world_bank_data as wb
-import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 from modules.nav import SideBarLinks
@@ -14,13 +12,33 @@ import requests
 SideBarLinks()
 
 # set the header of the page
-st.header('Adoptable Pet Data')
+st.header("Adoptable Pet Data Test!")
 
 # You can access the session state to make a more customized/personalized app experience
 st.write(f"### Hi, {st.session_state['first_name']}!")
-st.write('This is a list of pets that are alive and available for adoption.')
+st.write('Select your pet criteria:')
 pets = requests.get('http://api:4000/p/pets/available').json()
-st.dataframe(pets)
 
+# Add a multiselect widget to select species
+selected_species = st.multiselect('Preferred species:', list(set(item['species'] for item in pets)))
+
+# Add a multiselect widget to select breeds
+selected_breeds = st.multiselect('Preferred breeds:', list(set(item['breed'] for item in pets)))
+
+# Add a range slider for age
+selected_age = st.slider("Preferred age:", 0, 25, (0, 25))
+
+# filters pets data
+filtered_pets = pets
+if selected_species:
+    filtered_pets = [item for item in filtered_pets if item.get('species') in selected_species]
+if selected_breeds:
+    filtered_pets = [item for item in filtered_pets if item.get('breed') in selected_breeds]
+if selected_age:
+    filtered_pets = [item for item in filtered_pets if selected_age[0] <= int(item.get('age')) <= selected_age[1]]
+
+# Report the unfiltered/filtered dataframe
+st.write('Here are all the pets that fufill your criteria!')
+st.dataframe(filtered_pets)
 
     
