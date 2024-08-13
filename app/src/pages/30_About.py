1d0be46e420f17fe0c51f036aeb313264a319fbb
kaamil2 <kaamil22thobani@gmail.com>
2024-08-13 12:46:45 -0400

    merged with front end side should be workiing merging to main after with gabriella

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -6,13 +6,16 @@ SideBarLinks()
 
 st.write("# About this App")
 
+
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
-
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
-
+    This is a demo app for CS 3200 Course Project HomeFinder.  
+    This product provides buyers, sellers, and realtors with
+    market data and predictive analytics to determine the best
+    time to buy or sell a home. It features a user-friendly 
+    interface with a local area map and future home valuation 
+    predictions, offering a comprehensive solution for real 
+    estate needs. 
     Stay tuned for more information and features to come!
     """
         )
