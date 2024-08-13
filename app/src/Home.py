e405423470c9a0836191db961bb856a05930c00d
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-13 15:13:35 -0400

    edited theme of home page

--- app/src/Home.py
+++ app/src/Home.py
@@ -36,7 +36,7 @@ SideBarLinks(show_home=True)
 logger.info("Loading the Home page of the app")
 st.title('Project Pal')
 st.write('\n\n')
-st.write('### HI! As which user would you like to log in?')
+st.write('### Hi! Which user would you like to log in?')
 
 # For each of the user personas for which we are implementing
 # functionality, we put a button on the screen that the user 
