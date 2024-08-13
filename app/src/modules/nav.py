fb50d0d3cf0b594df60efd342299387ad01ecffd
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-13 15:06:26 -0400

    added new logo

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -42,7 +42,7 @@ def SideBarLinks(show_home=False):
     """    
 
     # add a logo to the sidebar always
-    st.sidebar.image("assets/logo.png", width = 150)
+    st.sidebar.image("assets/logo.png", width = 200)
 
     # If there is no logged in user, redirect to the Home (Landing) page
     if 'authenticated' not in st.session_state:
