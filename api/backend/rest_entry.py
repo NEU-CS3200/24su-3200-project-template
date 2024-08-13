edaa92c4638ede64c364bfdfc489d0b593954062
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-13 14:51:12 -0400

    added student availability route

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -7,7 +7,7 @@ from backend.db_connection import db
 #from backend.customers.customer_routes import customers
 #from backend.products.products_routes import products
 from backend.student.student_routes import students
-from backend.studentAvailability.studentAvailability_routes import studentAvailability
+from backend.stuAvailability.stuAvailability_routes import stuAvailability
 from backend.ta.ta_routes import ta
 import os
 from dotenv import load_dotenv
@@ -99,7 +99,7 @@ def create_app():
     #app.register_blueprint(customers,   url_prefix='/c')
     #app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(students,   url_prefix='/s')
-    app.register_blueprint(studentAvailability,   url_prefix='/sa')
+    app.register_blueprint(stuAvailability,   url_prefix='/sa')
     app.register_blueprint(ta,   url_prefix='/t')
 
     # Don't forget to return the app object
--- api/backend/stuAvailability/stuAvailability_routes.py
+++ api/backend/stuAvailability/stuAvailability_routes.py
@@ -1,24 +1,24 @@
 ########################################################
-# Sample ta blueprint of endpoints
+# Sample student blueprint of endpoints
 ########################################################
 from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from backend.db_connection import db
 from backend.ml_models.model01 import predict
 
-ta = Blueprint('ta', __name__)
+stuAvailability = Blueprint('stuAvailability', __name__)
 
-@ta.route('/ta', methods = ['GET'])
+@stuAvailability.route('/stuAvailability', methods = ['GET'])
 
-def get_all_ta():
-    current_app.logger.info('ta_routes.py: GET /ta')
+def get_all_stuAvailability():
+    current_app.logger.info('stuAvailability_routes.py: GET /stuAvailability')
     cursor = db.get_db().cursor()
     the_query = '''
-    SELECT ta_id, first_name, last_name, email, course_name, s.section_num, s.semester_year
-    FROM TA ta JOIN Section s ON ta.section_num=s.section_num
-    AND ta.semester_year=s.semester_year
-    AND ta.course_id=s.course_id
-    JOIN Class c ON c.course_id=s.course_id;
+    SELECT sa.availability_id, student_id, day, location, time
+    FROM StudentAvailability sa JOIN Availability a ON sa.availability_id=a.availability_id
+    JOIN Days d ON a.day_id=d.day_id
+    JOIN Location l ON a.location_id=l.location_id
+    JOIN Time t on a.time_id=t.time_id;
     '''
     cursor.execute(the_query)
     theData = cursor.fetchall()
