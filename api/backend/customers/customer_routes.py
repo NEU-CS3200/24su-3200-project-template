b1812ed9f4599c872973501f8df648fd6fb6dc12
Catherine McKinley <catherinemckinley1208@gmail.com>
2024-08-12 21:55:05 -0400

    changed some stuff from videos

--- api/backend/customers/customer_routes.py
+++ api/backend/customers/customer_routes.py
@@ -30,12 +30,13 @@ def get_customers():
     cursor = db.get_db().cursor()
     cursor.execute('select id, company, last_name,\
         first_name, job_title, business_phone from customers')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
+    #row_headers = [x[0] for x in cursor.description]
+    #json_data = []
     theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
+    #for row in theData:
+        #json_data.append(dict(zip(row_headers, row)))
+    #the_response = make_response(jsonify(json_data))
+    the_response = make_response(theData)
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
@@ -63,12 +64,13 @@ def get_customer(userID):
     current_app.logger.info('GET /customers/<userID> route')
     cursor = db.get_db().cursor()
     cursor.execute('select id, first_name, last_name from customers where id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
+    #row_headers = [x[0] for x in cursor.description]
+    #json_data = []
     theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
+    #for row in theData:
+       # json_data.append(dict(zip(row_headers, row)))
+    #the_response = make_response(jsonify(json_data))
+    the_response = make_response(theData)
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
