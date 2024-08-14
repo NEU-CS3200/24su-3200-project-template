18e4cac057c98258827d8b361c4cccce3bccd90c
hayliepedersen <pedersenhaylie6@gmail.com>
2024-08-14 10:43:43 -0400

    set up bar chart to display data

--- app/src/pages/21_Least_Adoptions.py
+++ app/src/pages/21_Least_Adoptions.py
@@ -4,7 +4,7 @@ import streamlit as st
 from modules.nav import SideBarLinks
 import requests
 import matplotlib.pyplot as plt
-import plotly.express as px
+import pandas as pd
 
 st.set_page_config(layout = 'wide')
 
@@ -18,8 +18,13 @@ st.write('### Hi Alex, here are the agencies with the least adoptions.')
 # Fetching the data from the api
 data = {} 
 try:
-  data = requests.get('http://api:4000/a/agencies').json()
+  data = requests.get('http://api:4000/a/petagencies').json()
 except:
   st.write("Could not connect to database to retrieve agencies!")
 
-st.dataframe(data)
+df = pd.DataFrame(data)
+
+# Sorting the data
+df_sorted = df.sort_values(by='Total_Adoptions')
+
+st.bar_chart(df_sorted, x="agencyName", y="Total_Adoptions", color="#89CFF0", horizontal=True)
