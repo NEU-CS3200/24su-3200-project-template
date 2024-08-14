b9c9d0e95dbc56b451dee34529433b3b5c3c1a3f
WILLCHRIS10 <christianto.w@northeastern.edu>
2024-08-13 21:14:28 -0400

    Clicks - Will - GET

--- api/backend/hotel_clicks/hotel_clicks_routes.py
+++ api/backend/hotel_clicks/hotel_clicks_routes.py
@@ -1,25 +1,20 @@
 from flask import Blueprint, request, jsonify, make_response, current_app
 from backend.db_connection import db
 
-# Create a new Blueprint named "clicks" for managing click-related routes
-restaurant_clicks = Blueprint('restautant_clicks', __name__)
+# Create a new Blueprint named hotel_clicks for managing click-related routes
+hotel_clicks = Blueprint('hotel_clicks', __name__)
 
-# Route to retrieve all clicks for restaurants
-@restaurant_clicks.route('/restaurant_clicks/alldata', methods=['GET'])
-def get_restaurant_clicks():
+# Route to retrieve all clicks for hotels
+@hotel_clicks.route('/hotel_clicks/alldata', methods=['GET'])
+def get_all_hotel_clicks():
     cursor = db.get_db().cursor()
 
-    # Execute a SQL query to fetch all records from the restaurant_clicks table
-    cursor.execute('SELECT restaurant_id, click_counter, user_id,clicked_at, restaurant_click_id FROM restaurant_clicks')
+    # Execute a SQL query to fetch all records from the hotel_clicks table
+    cursor.execute('SELECT hotel_id, click_counter, user_id, clicked_at, hotel_click_id FROM hotel_clicks')
     
     clicks_data = cursor.fetchall()
 
     the_response = make_response(jsonify(clicks_data))
     the_response.status_code = 200  
-    the_response.mimetype = 'application/json'
-    return the_response
-
-# Route to retrieve all clicks for restaurants
-@restaurant_clicks.route('/restaurant_clicks', methods=['GET'])
-def get_restaurant_clicks():
-    cursor = db.get_db().cursor()
+    the_response.mimetype = 'application/json'  
+    return the_response  
