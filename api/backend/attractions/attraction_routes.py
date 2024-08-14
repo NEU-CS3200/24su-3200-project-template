be04bfe7b530621696d4171268afea1f626dd0f8
Vero <62412647+VeroS-pdf@users.noreply.github.com>
2024-08-13 20:52:28 -0400

    minor changes to home and nav

--- api/backend/attractions/attraction_routes.py
+++ api/backend/attractions/attraction_routes.py
@@ -5,3 +5,9 @@ from backend.db_connection import db
 attractions = Blueprint("attractions", __name__)
 
 @attractions.route()
+def get_attractions():
+    the_query = '''
+        Select attraction_name
+        From attractions
+    '''
+    cursor.execute(the_query)
--- api/backend/flights/flight_routes.py
+++ api/backend/flights/flight_routes.py
@@ -5,9 +5,9 @@ from backend.db_connection import db
 flights = Blueprint("flights", __name__)
 
 # Gets all the flights that fit under a certain price range
-@flights.route('/price_range/max_price/min_price)', methods = ['GET'])
+@flights.route('/price_range/<max_price>/<min_price>)', methods = ['GET'])
 def price_range(max_price, min_price):
-    current_app.logger.info()
+    current_app.logger.info('flight_routes.py: GET based on budget')
     cursor = db.get_db().cursor()
     cursor.execute('select airline_name, price from flights')
 
