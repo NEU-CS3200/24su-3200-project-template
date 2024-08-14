9cbf1d270404e76890b1a7137cb427f7acdb7ddc
layzarodri <layzapr736@gmail.com>
2024-08-14 07:24:00 -0400

    Update ads_routes.py
    adding get and post method into ads_route.py

--- api/backend/ads/ads_routes.py
+++ api/backend/ads/ads_routes.py
@@ -5,12 +5,12 @@ from backend.ml_models.model01 import predict
 
 ads = Blueprint('ads', __name__)
 
-# Get all the ads type and description from a marketing campaign 
+# Get all the ads informations from a marketing campaign 
 @ads.route('/ads', methods =['GET'])
 def get_ads():
-    current_app.logger.info('customer_routes.py: GET /users')
+    current_app.logger.info('ads_routes.py: GET /users')
     cursor = db.get_db().cursor()
-    cursor.execute('select types, description from ads')
+    cursor.execute('select id, terms_and_conditions, description, types, description from ads')
     
     theData = cursor.fetchall()
     
@@ -18,3 +18,26 @@ def get_ads():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Adding a new ad 
+@employees.route('/ads', methods=['POST'])
+def add_new_employee():
+    the_data = request.json
+    current_app.logger.info(the_data)
+    id = the_data['id']
+    description = the_data['description']
+    types = the_data['types']
+    terms_and_conditions = the_data['terms_and_conditions']
+    budget = the_data['budget']
+
+    query = 'insert into ads (id, description, types, terms_and_conditions, budget) values("'
+    query += id + '", "'
+    query += description + '", "'
+    query += types + '", "' 
+    query += terms_and_conditions + '", "'
+    query += budget + ')'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db.commit()
+    return 'Success'
