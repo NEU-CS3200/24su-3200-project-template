a35d66b162631333076611ebdef420e38d9020a8
Laurennc33 <laurennc3323@gmail.com>
2024-08-14 19:06:05 -0400

    Fixed hotel route

--- api/backend/hotels/hotel_routes.py
+++ api/backend/hotels/hotel_routes.py
@@ -9,19 +9,21 @@ hotel = Blueprint('hotel', __name__)
 def get_hotel(destination):
     current_app.logger.info('hotel_routes.py: GET /hotel')
     cursor = db.get_db().cursor()
-    query = '''
-        Select hotel.id, hotel.room_type, hotel.amenities, hotel.price_per_night, hotel.rating, hotel.city_id 
+    query = f'''
+        Select hotel.id, hotel.room_type, hotel.amenities, hotel.price_per_night, hotel.rating, hotel.city_id
         FROM hotel JOIN trip ON hotel.city_id = trip.city_id
         JOIN city ON hotel.city_id = city.id
-        WHERE city.name = %s
-        AND (hotel.price_per_night * trip.group_size * trip.num_of_nights) <= trip.hotel_budget
-        ORDER BY rating, price_per_night DESC
+        WHERE city.name like "{destination}"
+        AND (hotel.price_per_night * trip.num_of_nights) <= trip.hotel_budget
+        ORDER BY rating, price_per_night DESC;
     '''
-    cursor.execute(query, (destination))
+    current_app.logger.info(f'query = {query}')
+    cursor.execute(query)
     
     theData = cursor.fetchall()
-    
+    current_app.logger.info(f'retVal = {theData}')
     the_response = make_response(theData)
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+    
