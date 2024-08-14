563ab7fbe21c9fac0314a27aeee768cba539db97
WILLCHRIS10 <christianto.w@northeastern.edu>
2024-08-13 21:03:26 -0400

    change

--- api/backend/restaurantclicks/restaurant_clicks_routes.py
+++ api/backend/restaurantclicks/restaurant_clicks_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 from backend.db_connection import db
 
 # Create a new Blueprint named restaurant clicks for managing click-related routes
-restaurant_clicks = Blueprint('restautant_clicks', __name__)
+restaurant_clicks = Blueprint('restaurant_clicks', __name__)
 
 # Route to retrieve all clicks for restaurants
 @restaurant_clicks.route('/restaurant_clicks/alldata', methods=['GET'])
