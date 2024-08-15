3b274d4fec9e0bad7abeb685c152175906b91ca8
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-15 12:38:34 -0400

    changed to home page

--- app/src/pages/20_Admin_Home.py
+++ app/src/pages/20_Admin_Home.py
@@ -18,9 +18,9 @@ st.write(f"Welcome, {st.session_state.get('first_name', 'Professor')}!")
 if st.button('Update groups', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/21_Model_Mgmt')
+  st.switch_page('pages/21_Model_Mgmt.py')
 
 if st.button('See my students', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/22_Section_Data')
+  st.switch_page('pages/22_Section_Data.py')
