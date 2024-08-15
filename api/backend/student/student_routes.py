d044c2f66fad0a59167f3d4e4c6ce2aa80f9a579
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-15 16:03:14 -0400

    got update major form to work again!

--- api/backend/student/student_routes.py
+++ api/backend/student/student_routes.py
@@ -8,8 +8,6 @@ from backend.ml_models.model01 import predict
 
 students = Blueprint('students', __name__)
 
-@students.route('/students', methods = ['GET'])
-def get_all_students():
 @students.route('/students/<speciality>', methods = ['POST','GET'])
 
 def get_all_students(speciality):
@@ -30,6 +28,41 @@ def get_all_students(speciality):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+@students.route('/students/<first_name>/<last_name>/<email>/<major>', methods=['PUT', 'GET'])
+def update_major(first_name, last_name, email, major):
+    current_app.logger.info('student_routes.py: GET /students/<first_name>/<last_name>/<email>/<major>')
+
+    # Establish database connection
+    connection = db.get_db()
+    cursor = connection.cursor()
+
+    # Use parameterized query to prevent SQL injection
+    query_id = '''SELECT student_id
+                FROM Student
+                WHERE first_name = %s
+                AND last_name = %s
+                AND email = %s'''
+    cursor.execute(query_id, (first_name, last_name, email))
+    theData = cursor.fetchone()
+
+    if theData:
+        student_id = theData['student_id']
+        
+        query_major = '''UPDATE Student
+                        SET major = %s
+                        WHERE student_id=%s'''
+        
+        cursor.execute(query_major, (major, student_id))
+        connection.commit()  # Commit the transaction
+        the_response = make_response(jsonify({'success': 'major updated successfully'}))
+    else:
+        # Return an error if the student is not found
+        the_response = make_response(jsonify({'error': 'not a student'}), 404)
+    
+    the_response.mimetype = 'application/json'
+    return the_response
+
 
 
 '''' @students.route('/avail/<email>', methods=['POST'])
