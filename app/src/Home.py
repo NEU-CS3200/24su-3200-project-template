721dcfec849322127a25c02dab3a86ec827162ad
cowang5 <coreyjwang03@gmail.com>
2024-08-15 13:49:43 -0400

    Update Home.py

--- app/src/Home.py
+++ app/src/Home.py
@@ -74,4 +74,5 @@ if st.button('Act as Mark, a NEU professor',
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'professor'
     st.session_state['first_name'] = 'Mark'
-    st.switch_page('pages/20_Professor_Home.py')
+    st.switch_page('pages/20_Admin_Home.py')
+    st.write('Hello')
