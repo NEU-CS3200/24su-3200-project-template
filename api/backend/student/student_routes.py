b99c2f1eef602c1f78a69179af3ef34f48c9a2d3
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-15 17:05:14 -0400

    added find on-campus!

--- api/backend/student/student_routes.py
+++ api/backend/student/student_routes.py
@@ -29,6 +29,7 @@ def get_all_students(speciality):
     the_response.mimetype = 'application/json'
     return the_response
 
+# this is the route to update a student's major 
 @students.route('/students/<first_name>/<last_name>/<email>/<major>', methods=['PUT', 'GET'])
 def update_major(first_name, last_name, email, major):
     current_app.logger.info('student_routes.py: GET /students/<first_name>/<last_name>/<email>/<major>')
@@ -63,6 +64,43 @@ def update_major(first_name, last_name, email, major):
     the_response.mimetype = 'application/json'
     return the_response
 
+# this route is to find students in the same section & live on campus 
+@students.route('/students/<email>/on-campus', methods=['GET'])
+def find_oc(email):
+    current_app.logger.info('student_routes.py: GET /students/<email>/on-campus')
+
+    # Establish database connection
+    connection = db.get_db()
+    cursor = connection.cursor()
+
+    # Use parameterized query to prevent SQL injection
+    query_id = '''SELECT student_id
+                FROM Student
+                WHERE email=%s'''
+    cursor.execute(query_id, (email))
+    theData = cursor.fetchone()
+
+    if theData:
+        student_id = theData['student_id']
+        
+        query_sec = '''SELECT c.course_name, ss.semester_year, ss.section_num, s.first_name, s.last_name, s.email, s.on_campus
+                    FROM StudentSection ss
+                    JOIN Student s ON ss.student_id = s.student_id
+                    JOIN Class c ON ss.course_id = c.course_id
+                    WHERE ss.section_num IN (SELECT section_num
+                    FROM StudentSection WHERE student_id = %s)
+                    AND ss.semester_year IN (SELECT semester_year
+                    FROM StudentSection WHERE student_id = %s)
+                    AND ss.course_id IN (SELECT course_id 
+                    FROM StudentSection WHERE student_id = %s)
+                    AND s.on_campus IS TRUE;'''
+        
+        cursor.execute(query_sec, (student_id, student_id, student_id))
+        theData = cursor.fetchall()
+        the_response = make_response(theData)
+        the_response.status_code = 200
+        the_response.mimetype = 'application/json'
+        return the_response
 
 
 '''' @students.route('/avail/<email>', methods=['POST'])
