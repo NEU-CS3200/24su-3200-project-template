095c17fcd55eba29d62c2e961d15a12afa11898f
cowang5 <coreyjwang03@gmail.com>
2024-08-15 14:29:28 -0400

    Update nav.py

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -82,7 +82,7 @@ def SideBarLinks(show_home=False):
 
     if st.session_state["authenticated"]:
         # Always show a logout button if there is a logged in user
-        if st.sidebar.button("Logout"):
+        if st.sidebar.button("Change User Type"):
             del st.session_state['role']
             del st.session_state['authenticated']
             st.switch_page('Home.py')
