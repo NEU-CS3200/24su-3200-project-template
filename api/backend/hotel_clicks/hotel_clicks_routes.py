4005959e3ba78beb9c7c810dd1bd3f5440d01f4b
Laurennc33 <laurennc3323@gmail.com>
2024-08-15 01:21:34 -0400

    edited hotel_clicks route to it prints the five hotel names with the most clicks

--- api/backend/hotel_clicks/hotel_clicks_routes.py
+++ api/backend/hotel_clicks/hotel_clicks_routes.py
@@ -5,14 +5,18 @@ from backend.db_connection import db
 hotel_clicks = Blueprint('hotel_clicks', __name__)
 
 # Route to retrieve the total clicks for a specific hotel
-@hotel_clicks.route('/hotel_clicks/<hotel_id>', methods=['GET'])
-def get_hotel_clicks(hotel_id):
-    current_app.logger.info(f'hotel_clicks.py: GET /hotel_clicks/{hotel_id}')
+@hotel_clicks.route('/hotel_clicks', methods=['GET'])
+def get_hotel_clicks():
+    current_app.logger.info(f'hotel_clicks.py: GET /hotel_clicks')
     cursor = db.get_db().cursor()
 
-    # Execute a SQL query to fetch the sum of clicks for the specified hotel_id
-    the_query = '''SELECT SUM(click_counter) AS total_clicks FROM hotel_clicks WHERE hotel_id = %s'''
-    cursor.execute(the_query, (hotel_id,))
+    # Execute a SQL query to fetch the sum of clicks 
+    the_query = '''SELECT name, SUM(click_counter) AS total_clicks 
+    FROM hotel_clicks join hotel ON hotel.id = hotel_clicks.hotel_id
+    GROUP BY name
+    ORDER BY total_clicks desc
+    Limit 5'''
+    cursor.execute(the_query)
 
     # Fetch the result, which should be a single value
     clicks_data = cursor.fetchone()
--- app/src/pages/Click_Count.py
+++ app/src/pages/Click_Count.py
@@ -2,6 +2,7 @@ import logging
 logger = logging.getLogger(__name__)
 import streamlit as st
 import requests
+import pandas as pd
 from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
 
@@ -9,3 +10,16 @@ SideBarLinks()
 
 st.write("Click Counter")
 
+
+# Fetch the data from the route
+url = "http://api:4000/hc/hotel_clicks"
+response = requests.get(url)
+response.raise_for_status()
+data_json = response.json()
+data = pd.DataFrame(data_json)
+
+# Streamlit app
+st.title('Hotel Clicks Data')
+
+# Display a bar chart
+st.bar_chart(data, x="name", y="hotel_clicks", color="site", stack=False)
